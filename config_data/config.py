e36ee1a94ec16b48ad7952b7f9b2899cfe158da7
PvlKr16 <102588315+PvlKr16@users.noreply.github.com>
2024-12-19 17:50:52 +0300

    Update config.py

--- config_data/config.py
+++ config_data/config.py
@@ -1,10 +1,7 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
-if not find_dotenv():
-    exit('Переменные окружения не загружены т.к отсутствует файл .env')
-else:
-    load_dotenv()
+load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
