29de10cdbed9b11bd57aefb94774ccb7ba3d1005
pavel <pavel_k@tut.by>
2024-12-21 23:09:21 +0300

    requirements.txt updated; main.py cleaned

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import re
 import telebot
 from googletrans import Translator, LANGUAGES
-from telebot import types, custom_filters
 from config_data import config
-from config_data.config import ADMIN_TELEGRAM_ID
 
 
 bot = telebot.TeleBot(token=config.BOT_TOKEN)
@@ -13,14 +10,12 @@ translator = Translator()
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     text_to_translate = message.text
-    # print(message)
     if translator.detect(text_to_translate).lang == "lt" and translator.detect(text_to_translate).confidence >= 0.80:
         translated_text = translator.translate(text_to_translate, dest="be").text
     elif translator.detect(text_to_translate).lang == "be" and translator.detect(text_to_translate).confidence >= 0.80:
         translated_text = translator.translate(text_to_translate, dest="lt").text
     else:
         translated_text = "something weird"
-    # print(translated_text)
     bot.reply_to(message, translated_text)
 
 # text_to_translate = "добрай раніцы"
